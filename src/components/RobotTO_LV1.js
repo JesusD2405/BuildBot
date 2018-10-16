4a350fc05ca8d890ccff800a29b9a17ff3f0e72c
Jesús David Pérez <jesusdavid2405@gmail.com>
2018-10-16 19:40:24 -0400

    Solucionando errores de mezcla en package.json y package-lock.json

--- src/components/RobotTO_LV1.js
+++ src/components/RobotTO_LV1.js
@@ -7,73 +7,126 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button, PanResponder, Animated, Dimensions} from 'react-native';
+
+import Draggable from 'react-native-draggable';
+
 
 export default class RobotTO_LV1 extends Component {
 
-  render() {
-	return (
-			
-		<View style={styles.content}>
+	constructor(props){
+	    super(props);
 
-			<View style={styles.titleBox}>
+	    this.state = {
+	    	showDraggable   : true,     
+        	dropZoneValues  : null,
+	        pan     : new Animated.ValueXY()   
+	    };
 
-				<Text style={styles.title}> Armar Robot </Text>
+	    this.panResponder = PanResponder.create({
 
-			</View>
-
-			<View style={styles.piecesBox}>
-
-			</View>
-
-			<View style={styles.robotBox}>
-
-			</View>
-
-		</View>	
-	);
-  }
-}
-
-const styles = StyleSheet.create({
-
-	content: {
-		flex: 1,
-		flexDirection: 'column',
-		backgroundColor: '#158FBF',
-	},
-
-	// Contenedor del titulo de la pantalla
-	titleBox: {
-		flexDirection: 'row',
-		backgroundColor: '#0277bd',
-		//justifyContent: 'center', // 
-		//alignItems: 'center',
-		//width: 150,
-		height: 80,
-	},
-
-	// Texto titulo de la pantalla
-	title: {
-		fontSize: 40,
-		paddingTop: 10,
-		color: 'white',
-		fontWeight: 'bold',
-		fontFamily: 'Cochin',
-    },
-
-    piecesBox:
-    {
-    	backgroundColor: 'red',
-    	width: 100,
-		height: 100,
-
-    },
-	robotBox:
-	{
-		backgroundColor: 'blue',
-		width: 100,
-		height: 100,
+    	onStartShouldSetPanResponder : () => true,
+	        onPanResponderMove           : Animated.event([null,{ 
+	            dx : this.state.pan.x,
+	            dy : this.state.pan.y
+	        }]),
+        
+        onPanResponderRelease           : (e, gesture) => {
+            if(this.isDropZone(gesture)){ 
+                this.setState({
+                    showDraggable : false 
+                });
+            }else{
+                Animated.spring(
+                    this.state.pan,
+                    {toValue:{x:0,y:0}}
+                ).start();
+            }
+        }
+    });
 	}
 
-,});
+	isDropZone(gesture){     
+	    var dz = this.state.dropZoneValues;
+	    return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+	}
+
+	setDropZoneValues(event){      
+	    this.setState({
+	        dropZoneValues : event.nativeEvent.layout
+	    });
+	}
+
+	render(){
+        return (
+            <View style={styles.mainContainer}>
+	            <View 
+	                onLayout={this.setDropZoneValues.bind(this)}     
+	                style={styles.dropZone}>
+	                <Text style={styles.text}>Zona 1</Text>
+	            </View>
+
+	            {this.renderDraggable()}
+	        </View>
+        );
+    }
+
+    renderDraggable(){
+    	if(this.state.showDraggable){
+		    return (
+		        <View style={styles.draggableContainer}>
+		            <Animated.View 
+		                {...this.panResponder.panHandlers}                       
+		                style={[this.state.pan.getLayout(), styles.circle]}>     
+		                <Text style={styles.text}>Pieza 1</Text>
+		            </Animated.View>
+
+		            <Animated.View 
+		                {...this.panResponder.panHandlers}                       
+		                style={[this.state.pan.getLayout(), styles.circle]}>     
+		                <Text style={styles.text}>Pieza 2</Text>
+		            </Animated.View>
+
+		            <Animated.View 
+		                {...this.panResponder.panHandlers}                       
+		                style={[this.state.pan.getLayout(), styles.circle]}>     
+		                <Text style={styles.text}>Pieza 3</Text>
+		            </Animated.View>
+
+		        </View>
+    		);
+    	}
+	}
+
+
+}
+
+let CIRCLE_RADIUS = 36;
+let Window = Dimensions.get('window');
+let styles = StyleSheet.create({
+    mainContainer: {
+        flex    : 1
+    },
+    dropZone    : {
+        height         : 100,
+        backgroundColor:'#2c3e50'
+    },
+    text        : {
+        marginTop   : 25,
+        marginLeft  : 5,
+        marginRight : 5,
+        textAlign   : 'center',
+        color       : '#fff'
+    },
+    draggableContainer: {
+        position    : 'absolute',
+        top         : Window.height/2 - CIRCLE_RADIUS,
+        left        : Window.width/2 - CIRCLE_RADIUS,
+    },
+    circle      : {
+        backgroundColor     : '#1abc9c',
+        width               : CIRCLE_RADIUS*2,
+        height              : CIRCLE_RADIUS*2,
+        borderRadius        : CIRCLE_RADIUS
+    }
+});
